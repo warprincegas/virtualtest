77ae46ca38da148749adef884883b6bb8b841a1e
warprincegas <warprincegas@gmail.com>
2024-10-29 02:20:59 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,21 +9,20 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000,
     plugins: [Inspect()],
     rollupOptions: {
-      // input: {
-      //   main: resolve(__dirname, "index.html"),
-      //   abuja: resolve(__dirname, "abuja/index.html"),
-      //   calabar: resolve(__dirname, "calabar/index.html"),
-      //   obudu: resolve(__dirname, "obudu/index.html"),
-      //   oldestC: resolve(__dirname, "oldest-church/index.html"),
-      // },
+      input: {
+        main: resolve(__dirname, "index.html"),
+        abuja: resolve(__dirname, "abuja/index.html"),
+        calabar: resolve(__dirname, "calabar/index.html"),
+        obudu: resolve(__dirname, "obudu/index.html"),
+        oldestC: resolve(__dirname, "oldest-church/index.html"),
+      },
       output: {
         manualChunks(id) {
-          // Custom chunking for dependencies
           if (id.includes("node_modules")) {
-            return "vendor"; // Bundles all dependencies from node_modules in a single 'vendor' chunk
+            return "vendor"; 
           }
           if (id.includes("src/components")) {
-            return "components"; // Groups components into a separate chunk
+            return "components"; 
           }
         },
       },
