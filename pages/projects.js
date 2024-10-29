b28255082428e19ae9f31a1e8aeb9a16c07a450f
warprincegas <warprincegas@gmail.com>
2024-10-29 02:24:36 +0100

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -1,3 +1,216 @@
 export default function Project() {
-  return `      `;
+  return `      <section
+        class="project mt-16 h-[50vh] md:mt-0 bg-center md:h-[60vh] lg:h-[80vh]"
+      >
+        <div
+          class="bg-[rgba(23,23,23,.7)] lg:bg-[rgba(23,23,23,.75)] h-full flex justify-center items-start flex-col gap-4 p-8 md:items-center md:bg-gradient-to-b from-[rgba(23,23,23,7)] to-[rgba(23,23,23,.5)]"
+        >
+          <span
+            class="font-medium text-sm mt-[10px] md:text-lg md:text-center tracking-wide block"
+          >
+            OUR WORKS
+          </span>
+          <h1 class="font-bold text-4xl tracking-wider">Projects</h1>
+        </div>
+      </section>
+      <section class="bg-black flex justify-center items-center py-[]2px">
+        <ul class="grid grid-cols-2 gap-x-16 gap-y-6 list-disc lg:flex">
+          <!-- <li class="cursor-pointer">All Items</li>
+          <li class="cursor-pointer">Education</li>
+          <li class="cursor-pointer">Hospitality</li>
+          <li class="cursor-pointer">Tourism</li> -->
+        </ul>
+      </section>
+      <main
+        class="text-stone-100 sm:grid sm:grid-cols-1 sm:gap-y-4 bg-black lg:grid-cols-3 sm:px-[10%] sm:py-10 sm:mb-[-1px] sm:gap-10 md:grid-cols-2 md:px-2 md:gap-2 md:py-2"
+      >
+        <a href="abuja/index.html">
+          <section class="ncm h-[400px] cursor-pointer font-sans sm:rounded-lg">
+            <div class="bg-[rgba(23,23,23,.8)] h-full py-6 px-3 sm:rounded-lg">
+              <div
+                class="flex-col gap-5 flex justify-center items-center h-full translate-y-[40px] project-content transition-all duration-500 p-7"
+              >
+                <h1
+                  class="font-medium text-2xl tracking-wider w-full translate-y-[5px] duration-300 project-name sm:font-bold"
+                >
+                  Abuja Central Mosques
+                </h1>
+                <span class="tracking-wider text-base sm:text-sm">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Provident similique repudiandae ullam earum iure adipisci, ut,
+                  exercitationem eius quidem, accusantium atque iste animi eum
+                  doloribus amet natus et ex! Veritatis?
+                </span>
+                <div
+                  class="flex items-center w-full gap-1 text-sm opacity-0 duration-500 project-link mt-3"
+                >
+                  <div class="">VIEW PROJECT</div>
+                  <ion-icon
+                    name="arrow-forward-circle-outline"
+                    class="text-base"
+                  ></ion-icon>
+                </div>
+              </div>
+            </div>
+          </section>
+        </a>
+        <a href="calabar/">
+          <section
+            class="calaber h-[400px] cursor-pointer font-sans rounded-lg sm:rounded-lg"
+          >
+            <div class="bg-[rgba(23,23,23,.8)] h-full p-6 sm:rounded-lg">
+              <div
+                class="flex-col gap-5 flex justify-center items-start h-full translate-y-[40px] project-content transition-all duration-500 py-6 px-3"
+              >
+                <h1
+                  class="font-medium text-2xl tracking-wider translate-y-[5px] duration-300 project-name sm:font-bold"
+                >
+                  Calaber
+                </h1>
+                <span class="tracking-wider text-base sm:text-sm">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Provident similique repudiandae ullam earum iure adipisci, ut,
+                  exercitationem eius quidem, accusantium atque iste animi eum
+                  doloribus amet natus et ex! Veritatis?
+                </span>
+                <div
+                  class="flex items-center w-full gap-1 text-sm opacity-0 duration-500 project-link mt-3"
+                >
+                  <div class="">VIEW PROJECT</div>
+                  <ion-icon
+                    name="arrow-forward-circle-outline"
+                    class="text-base"
+                  ></ion-icon>
+                </div>
+              </div>
+            </div>
+          </section>
+        </a>
+        <a href="#">
+          <section
+            class="mary-slessor h-[400px] cursor-pointer font-sans sm:rounded-lg"
+          >
+            <div class="bg-[rgba(23,23,23,.8)] h-full p-6 sm:rounded-lg">
+              <div
+                class="flex-col gap-5 flex justify-center items-start h-full translate-y-[40px] project-content transition-all duration-500 py-6 px-3"
+              >
+                <h1
+                  class="font-medium text-2xl tracking-wider translate-y-[5px] duration-300 project-name sm:font-bold"
+                >
+                  Mary Slessor
+                </h1>
+                <span class="tracking-wider text-base sm:text-sm">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Provident similique repudiandae ullam earum iure adipisci, ut,
+                  exercitationem eius quidem, accusantium atque iste animi eum
+                  doloribus amet natus et ex! Veritatis?
+                </span>
+                <div
+                  class="flex items-center w-full gap-1 text-sm opacity-0 duration-500 project-link mt-3"
+                >
+                  <div class="">VIEW PROJECT</div>
+                  <ion-icon
+                    name="arrow-forward-circle-outline"
+                    class="text-base"
+                  ></ion-icon>
+                </div>
+              </div>
+            </div>
+          </section>
+        </a>
+        <a href="obudu/index.html">
+          <section
+            class="obudu h-[400px] cursor-pointer font-sans sm:rounded-lg"
+          >
+            <div class="bg-[rgba(23,23,23,.8)] h-full p-6 sm:rounded-lg">
+              <div
+                class="flex-col gap-5 flex justify-center items-start h-full translate-y-[40px] project-content transition-all duration-500 py-6 px-3"
+              >
+                <h1
+                  class="font-medium text-2xl tracking-wider translate-y-[5px] duration-300 project-name sm:font-bold"
+                >
+                  Obudu
+                </h1>
+                <span class="tracking-wider text-base sm:text-sm">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Provident similique repudiandae ullam earum iure adipisci, ut,
+                  exercitationem eius quidem, accusantium atque iste animi eum
+                  doloribus amet natus et ex! Veritatis?
+                </span>
+                <div
+                  class="flex items-center w-full gap-1 text-sm opacity-0 duration-500 project-link mt-3"
+                >
+                  <div class="">VIEW PROJECT</div>
+                  <ion-icon
+                    name="arrow-forward-circle-outline"
+                    class="text-base"
+                  ></ion-icon>
+                </div>
+              </div>
+            </div>
+          </section>
+        </a>
+        <a href="oldest-church/">
+          <section class="oc h-[400px] cursor-pointer font-sans sm:rounded-lg">
+            <div class="bg-[rgba(23,23,23,.8)] h-full p-6 sm:rounded-lg">
+              <div
+                class="flex-col gap-5 flex justify-center items-start h-full translate-y-[40px] project-content transition-all duration-500 py-6 px-3"
+              >
+                <h1
+                  class="font-medium text-2xl tracking-wider translate-y-[5px] duration-300 project-name sm:font-bold"
+                >
+                  Oldest Church
+                </h1>
+                <span class="tracking-wider text-base sm:text-sm">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Provident similique repudiandae ullam earum iure adipisci, ut,
+                  exercitationem eius quidem, accusantium atque iste animi eum
+                  doloribus amet natus et ex! Veritatis?
+                </span>
+                <div
+                  class="flex items-center w-full gap-1 text-sm opacity-0 duration-500 project-link mt-3"
+                >
+                  <div class="">VIEW PROJECT</div>
+                  <ion-icon
+                    name="arrow-forward-circle-outline"
+                    class="text-base"
+                  ></ion-icon>
+                </div>
+              </div>
+            </div>
+          </section>
+        </a>
+        <a href="#">
+          <section
+            class="oshogbo-groove h-[400px] cursor-pointer font-sans sm:rounded-lg"
+          >
+            <div class="bg-[rgba(23,23,23,.8)] h-full p-6 sm:rounded-lg">
+              <div
+                class="flex-col gap-5 flex justify-center items-start h-full translate-y-[40px] project-content transition-all duration-500 py-6 px-3"
+              >
+                <h1
+                  class="font-medium text-2xl tracking-wider translate-y-[5px] duration-300 project-name sm:font-bold"
+                >
+                  Oshogbo Groove
+                </h1>
+                <span class="tracking-wider text-base sm:text-sm">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Provident similique repudiandae ullam earum iure adipisci, ut,
+                  exercitationem eius quidem, accusantium atque iste animi eum
+                  doloribus amet natus et ex! Veritatis?
+                </span>
+                <div
+                  class="flex items-center w-full gap-1 text-sm opacity-0 duration-500 project-link mt-3"
+                >
+                  <div class="">VIEW PROJECT</div>
+                  <ion-icon
+                    name="arrow-forward-circle-outline"
+                    class="text-base"
+                  ></ion-icon>
+                </div>
+              </div>
+            </div>
+          </section>
+        </a>
+      </main>  `;
 }
